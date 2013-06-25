531855593c55ed2d62af6033de68ff94d1d9c01d
arka-g <gangua@mcmaster.ca>
2013-06-24 23:44:18 -0300

    updates variable naming

--- gradecalc.py
+++ gradecalc.py
@@ -5,30 +5,30 @@ from graphics import *
 #main function
 def main():
     name = raw_input("Enter filename(grades): ") 
-    U,A = processFile(name) #stores value from the processfile function
-    win = GraphWin("Year 1 Grades", 500,200)    #size of graphics window
-    outputGUI(win,U,A)
+    Units,Avg = processFile(name) #stores value from the processfile function
+    win = GraphWin("Grades", 500,200)    #size of graphics window
+    outputGUI(win,Units,Avg)
 
 def processFile(name):
     file1 = open(name, 'r') #opens file (to be read)
     data1 = file1.read()
     data1 = data1.split("\n") #split data by new lines
 
-    U = 0
+    Units = 0
     for i in data1:
         j = i.split("\t")   #loops through by going through splitting by tabs
-        U = int(j[1])+ U #counts the units of each course
+        Units = int(j[1])+ Units #counts the units of each course
 
     B = 0
     for y in data1:
         k = y.split("\t")
         B = B + (int(k[1])*int(k[2])) #multiplies units and grade of each course
         B = float(B)
-    A = B/U #divides by total number of units to get average
-    return U,A
+    Avg = B/Units #divides by total number of units to get average
+    return Units,Avg
 
-def outputGUI(win,U,A):
-    sentence = Text(Point(250,110),"Units = "+str(U)+", Average = "+str(A))
+def outputGUI(win,Units,Avg):
+    sentence = Text(Point(250,110),"Units = "+str(Units)+", Average = "+str(Avg))
     sentence.setSize(15)
     sentence.setTextColor("purple")
     sentence.draw(win)
